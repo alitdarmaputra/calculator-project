f92861c2c44eacf0a716e86956d2e773d2693397
alitdarmaputra <alitdarmaputra@gmail.com>
2022-01-08 13:23:44 +0800

    add Append Number Function for Current Display

---
+++ script.js
@@ -0,0 +1,40 @@
+class Calculator {
+
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operator = undefined;
+    }
+
+    appendNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    updateDisplay() {
+        console.info(this.currentOperand);
+        this.currentOperandTextElement.innerText = this.currentOperand;
+    }
+}
+
+const numberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals");
+const deleteButton = document.querySelector(".delete");
+const allClearButton = document.querySelector(".all-clear");
+const previousOperandTextElement = document.querySelector(".previous-operand");
+const currentOperandTextElement = document.querySelector(".current-operand");
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButton.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
